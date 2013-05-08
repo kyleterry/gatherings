60498c38aa2d1b3ef44dbf76f478c20adad151c6
Kyle Terry <kyle@kyleterry.com>
2013-05-07 23:28:30 -0700

    clickable news tags

--- gatherings/news/urls.py
+++ gatherings/news/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('gatherings.news.views',
+    url(r'^news/by-tag/(?P<tag>[-\w]+)/$', 'list_by_tag', name='news_list_by_tag'),
     url(r'^news/(?P<post_id>\d)/', 'view_post', name='news_view_post'),
-    url(r'^news/', 'list_all', name='news_list'),
+    url(r'^news/$', 'list_all', name='news_list'),
 )
--- gatherings/news/views.py
+++ gatherings/news/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import get_object_or_404
 from annoying.decorators import render_to
 
-from gatherings.news.models import Post
+from gatherings.news.models import Post, Tag
 
 
 @render_to('news/list.html')
 def list_all(request):
     news = Post.objects.published.order_by('-created_at')
+    return locals()
+
+
+@render_to('news/list.html')
+def list_by_tag(request, tag):
+    tag = get_object_or_404(Tag, name=tag)
+    news = Post.objects.published.filter(tags=tag).order_by('-created_at')
     return locals()
 
 
